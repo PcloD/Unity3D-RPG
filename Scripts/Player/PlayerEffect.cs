5ae9f1c208d6d7c4d3d37596171d7e4c66834746
tkonexhh <410830616@qq.com>
2016-10-12 17:32:56 +0800

    Add files via upload

--- Scripts/Player/PlayerEffect.cs
+++ Scripts/Player/PlayerEffect.cs
@@ -5,29 +5,56 @@ public class PlayerEffect : MonoBehaviour {
 
 	private Renderer[] renderArray;
 	private NcCurveAnimation ac;
+    private GameObject EffectOffset;
 
 	// Use this for initialization
 	void Start () {
 		renderArray = this.GetComponentsInChildren<Renderer> ();
 		ac = this.GetComponentInChildren<NcCurveAnimation> ();
+
+
+        if (transform.Find("EffectOffset")!=null)
+        {
+            EffectOffset = transform.Find("EffectOffset").gameObject;
+        }
+        
 	}
 	
 	public void Show()
 	{
 		Debug.Log("Show Effect");
 
-		foreach (Renderer render in renderArray) {
-			render.enabled=true;
 
-		}
+        if (EffectOffset != null)
+        {
+            EffectOffset.SetActive(true);
+            StartCoroutine(DisableEffect(EffectOffset, 1f));
+        }
+        else
+        {
+            foreach (Renderer render in renderArray)
+            {
+                render.enabled = true;
 
-		ac.ResetAnimation ();
+            }
+
+            ac.ResetAnimation();
+        }
+        
+		
 	}
 
 	void Update()
 	{
-		if (Input.GetMouseButtonDown (0)) {
-			Show();
-		}
+        //if (Input.GetMouseButtonDown (0)) {
+        //    //Show();
+        //}
 	}
+
+
+    IEnumerator DisableEffect(GameObject go,float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        go.SetActive(false);
+    }
 }
